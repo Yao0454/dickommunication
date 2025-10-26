1e717fe06940ddca36a5ef0a07e03501bd458112
Yao0454 <yaoyifeng20070524@outlook.com>
2025-10-26 11:12:51 +0800

    Update

---
+++ include/server/yyfshead.h
@@ -0,0 +1,15 @@
+#ifndef YYFSHEAD_H
+#define YYFSHEAD_H
+
+#include <winsock2.h>
+#include <iostream>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+//我受不了写std::了 FUUUUUUUUCKKKK
+
+#endif
